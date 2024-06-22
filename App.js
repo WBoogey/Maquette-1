473729e5c5ccf4e808e28bd6066e0e18959024e3
Wboogey <ehourathegreat@gmail.com>
2024-06-22 12:15:56 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const Hamburger = document.querySelector(".hamburger")
+
+const NavLinks = document.querySelector(".navlinks")
+
+
+const Toggle = () =>{
+    NavLinks.classList.toggle('open')
+    if(NavLinks.classList.contains('open')){
+        document.querySelector(".hamburger .material-symbols-outlined").innerHTML= "close"
+    }else{
+        document.querySelector(".hamburger .material-symbols-outlined").innerHTML= "menu"
+    }
+}
+
+Hamburger.addEventListener("click", Toggle)
